d8c945694a466e77107c3afc9b1f5a4be58cd7e8
wcmckee <will@artcontrol.me>
2014-08-02 23:35:40 +0000

    rgd

--- index.py
+++ index.py
@@ -63,8 +63,181 @@ with nbdoc:
 
 print nbdoc
 
-# <codecell>
+# <markdowncell>
 
+# <!DOCTYPE html>
+# <html>
+#   <head>
+#     <title>BroBeur Notebooks</title>
+#     <link href="style.css" rel="stylesheet">
+#     <script src="script.js" type="text/javascript"></script>
+#   </head>
+#   <body>
+#     <div id="header">
+#       <ol>
+#         <li>
+#           <a href="/wcmgit.html">wcmgit.html</a>
+#         </li>
+#         <li>
+#           <a href="/tlc.html">tlc.html</a>
+#         </li>
+#         <li>
+#           <a href="/digzocean.html">digzocean.html</a>
+#         </li>
+#         <li>
+#           <a href="/tpb.html">tpb.html</a>
+#         </li>
+#         <li>
+#           <a href="/bugit.html">bugit.html</a>
+#         </li>
+#         <li>
+#           <a href="/sortbooks.html">sortbooks.html</a>
+#         </li>
+#         <li>
+#           <a href="/adypost.html">adypost.html</a>
+#         </li>
+#         <li>
+#           <a href="/pyEmailz.html">pyEmailz.html</a>
+#         </li>
+#         <li>
+#           <a href="/nbconv.html">nbconv.html</a>
+#         </li>
+#         <li>
+#           <a href="/master.html">master.html</a>
+#         </li>
+#         <li>
+#           <a href="/pydog.html">pydog.html</a>
+#         </li>
+#         <li>
+#           <a href="/spellcheckin.html">spellcheckin.html</a>
+#         </li>
+#         <li>
+#           <a href="/jsonBehind.html">jsonBehind.html</a>
+#         </li>
+#         <li>
+#           <a href="/hamgar.html">hamgar.html</a>
+#         </li>
+#         <li>
+#           <a href="/markdown.html">markdown.html</a>
+#         </li>
+#         <li>
+#           <a href="/wcmdocker.html">wcmdocker.html</a>
+#         </li>
+#         <li>
+#           <a href="/wirePIL.html">wirePIL.html</a>
+#         </li>
+#         <li>
+#           <a href="/netcafe.html">netcafe.html</a>
+#         </li>
+#         <li>
+#           <a href="/tarpipe.html">tarpipe.html</a>
+#         </li>
+#         <li>
+#           <a href="/ftpWCM.html">ftpWCM.html</a>
+#         </li>
+#         <li>
+#           <a href="/loop.html">loop.html</a>
+#         </li>
+#         <li>
+#           <a href="/rgdsnatch.html">rgdsnatch.html</a>
+#         </li>
+#         <li>
+#           <a href="/metatime.html">metatime.html</a>
+#         </li>
+#         <li>
+#           <a href="/skins.html">skins.html</a>
+#         </li>
+#         <li>
+#           <a href="/getdrawn.html">getdrawn.html</a>
+#         </li>
+#         <li>
+#           <a href="/Untitled0.html">Untitled0.html</a>
+#         </li>
+#         <li>
+#           <a href="/NumPyExplore.html">NumPyExplore.html</a>
+#         </li>
+#         <li>
+#           <a href="/openimg.html">openimg.html</a>
+#         </li>
+#         <li>
+#           <a href="/Untitled1.html">Untitled1.html</a>
+#         </li>
+#         <li>
+#           <a href="/Game Of Thrones.html">Game Of Thrones.html</a>
+#         </li>
+#         <li>
+#           <a href="/pyatakl.html">pyatakl.html</a>
+#         </li>
+#         <li>
+#           <a href="/json.html">json.html</a>
+#         </li>
+#         <li>
+#           <a href="/pyssh.html">pyssh.html</a>
+#         </li>
+#         <li>
+#           <a href="/url redirect.html">url redirect.html</a>
+#         </li>
+#         <li>
+#           <a href="/whai.html">whai.html</a>
+#         </li>
+#         <li>
+#           <a href="/test.html">test.html</a>
+#         </li>
+#         <li>
+#           <a href="/utils.html">utils.html</a>
+#         </li>
+#         <li>
+#           <a href="/karen.html">karen.html</a>
+#         </li>
+#         <li>
+#           <a href="/artcontrol.html">artcontrol.html</a>
+#         </li>
+#         <li>
+#           <a href="/pynztacam.html">pynztacam.html</a>
+#         </li>
+#         <li>
+#           <a href="/wallpapersav.html">wallpapersav.html</a>
+#         </li>
+#         <li>
+#           <a href="/redTube.html">redTube.html</a>
+#         </li>
+#         <li>
+#           <a href="/hackbrobeur.html">hackbrobeur.html</a>
+#         </li>
+#         <li>
+#           <a href="/flask-test.html">flask-test.html</a>
+#         </li>
+#         <li>
+#           <a href="/opche.html">opche.html</a>
+#         </li>
+#         <li>
+#           <a href="/index.html">index.html</a>
+#         </li>
+#         <li>
+#           <a href="/scrapnot.html">scrapnot.html</a>
+#         </li>
+#         <li>
+#           <a href="/reddit.html">reddit.html</a>
+#         </li>
+#         <li>
+#           <a href="/wordpress.html">wordpress.html</a>
+#         </li>
+#         <li>
+#           <a href="/test-wp.html">test-wp.html</a>
+#         </li>
+#         <li>
+#           <a href="/aklmcam.html">aklmcam.html</a>
+#         </li>
+#         <li>
+#           <a href="/webData.html">webData.html</a>
+#         </li>
+#       </ol>
+#     </div>
+#     <div class="body">
+#       <p>Lorem ipsum..</p>
+#     </div>
+#   </body>
+# </html>
 
 # <codecell>
 
---
+++ rgdsnatch.py
@@ -0,0 +1,392 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <codecell>
+
+import os
+import random
+import requests
+import re
+import json
+import praw
+import dominate
+from dominate.tags import *
+
+# <codecell>
+
+os.chdir('/home/wcmckee/artcontroldrawsyou')
+
+# <codecell>
+
+r = praw.Reddit(user_agent='rgdsnatch')
+
+# <codecell>
+
+r.login('artcontrol', 'zipit123')
+
+# <codecell>
+
+usrd = r.get_my_subreddits()
+
+# <codecell>
+
+suls = []
+
+# <codecell>
+
+for subs in usrd:
+    print subs
+    suls.append(subs)
+
+# <codecell>
+
+suls
+
+# <codecell>
+
+lesuls = len(suls)
+
+ransuz = random.randint(0, lesuls)
+
+thesubraz = suls[ransuz]
+
+# <codecell>
+
+suls
+
+# <codecell>
+
+print thesubraz
+
+# <codecell>
+
+rd = r.get_subreddit('redditgetsdrawn')
+
+# <codecell>
+
+rdnewz = rd.get_new()
+
+# <codecell>
+
+rdnew = []
+
+# <codecell>
+
+rdnew
+
+# <codecell>
+
+for uz in rdnewz:
+    #print uz
+    rdnew.append(uz)
+
+# <codecell>
+
+urzlis = []
+
+# <codecell>
+
+os.chdir('imgs')
+
+# <codecell>
+
+for rdn in rdnew:
+    print rdn.url
+    urzlis.append(rdn.url)
+    
+
+# <codecell>
+
+jplis = []
+
+# <codecell>
+
+for urz in urzlis:
+    if '.jpg' in urz:
+        jplis.append(urz)
+
+# <codecell>
+
+doc = dominate.document(title='artcontroldrawsyou')
+
+# <codecell>
+
+with doc.head:
+    link(rel='stylesheet', href='style.css')
+    script(type='text/javascript', src='script.js')
+
+with doc:
+    with div(id='header').add(p()):
+        for i in jplis:
+            li(img(i.lower(), src='%s' % i))
+            li(a(i.lower(), href='%s' % i))
+
+    with div():
+        attr(cls='body')
+        p('Lorem ipsum..')
+
+print doc
+
+# <codecell>
+
+ophtml = open('index.html', 'w')
+
+# <codecell>
+
+ophtml.write(str(doc))
+
+# <codecell>
+
+ophtml.close()
+
+# <codecell>
+
+cat index.html
+
+# <codecell>
+
+jplis
+
+# <codecell>
+
+rdnew
+
+# <codecell>
+
+ransubz = random.randint(0,24)
+
+# <codecell>
+
+print ransubz
+
+# <codecell>
+
+ransev = rdnew[ransubz]
+
+# <codecell>
+
+rgdautoz = str(ransev.author)
+
+# <codecell>
+
+rgdsubred = str(ransev.subreddit)
+
+# <codecell>
+
+sutit = ('[' + rgdsubred + ']')
+
+# <codecell>
+
+sutit
+
+# <codecell>
+
+rgdaqwew = ('xpost[RGD]' + rgdautoz)
+
+# <codecell>
+
+rgdaturo = str(ransev.url)
+
+# <codecell>
+
+rgdatit = str(ransev.title)
+
+# <codecell>
+
+rgdatit
+
+# <codecell>
+
+#rd.get_top
+
+# <codecell>
+
+linkdict = {}
+
+# <codecell>
+
+ls
+
+# <codecell>
+
+ady = r.get_subreddit('artcontroldrawsyou')
+
+# <codecell>
+
+comrgd =  rgdatit + ' ' + rgdaturo
+
+# <codecell>
+
+comrgd
+
+# <codecell>
+
+import time
+
+# <codecell>
+
+loctime = time.localtime()
+
+# <codecell>
+
+loctime.tm_mday
+
+# <codecell>
+
+loctime.tm_mon
+
+# <codecell>
+
+thedat = (str(loctime.tm_mday) + '/' + str(loctime.tm_mon))
+
+# <codecell>
+
+thedat
+
+# <codecell>
+
+mixtut = (thedat + sutit + rgdautoz)
+
+# <codecell>
+
+loctime
+
+# <codecell>
+
+ady.submit(mixtut , (rgdaqwew, comrgd))
+
+# <rawcell>
+
+# for newa in rdnew:
+#     #rint newa.url
+#     print len(newa)
+#     htmstr = (str(newa.title) + '<a href="' + 
+#                  str(newa.url) + 
+#                  '"><img class="aligncenter size-large wp-image-5723" alt="' +
+#                  str(newa.author) +
+#                  '" src="' + 
+#                  str(newa.url) + 
+#                  '" /></a>')
+#     #ophtml.write(htmstr)
+#     #ady.submit(('[RGD]' + newa.author), newa.url)
+#     print newa.author
+#     #print newa.media
+#     ophtml.write(htmstr)
+#     print newa.selftext
+#     print newa.url
+#     print newa.num_comments
+#     
+#     linkdict.update({str(newa.author): str(newa.url)})
+
+# <codecell>
+
+#print str(newa.title)
+
+# <codecell>
+
+import json
+
+# <codecell>
+
+newzjson = json.dumps(linkdict)
+
+# <codecell>
+
+#newzjson
+
+# <codecell>
+
+
+# <codecell>
+
+rmine = r.get_redditor('itwillbemine')
+
+# <codecell>
+
+#opest = open('userurl.json', 'r')
+#opest.read()
+#opest.close()
+
+# <codecell>
+
+mincom = rmine.get_comments()
+
+# <codecell>
+
+#print mincom
+
+# <codecell>
+
+minels = []
+
+# <codecell>
+
+
+dausr = {}
+
+# <codecell>
+
+for newa in rdnew:
+    #rint newa.url
+    #print newa.author
+    linkdict.update({str(newa.author): str(newa.url)})
+
+# <codecell>
+
+for con in mincom:
+    #print con.body
+    minels.append(con)
+    dausr.update({str(con.id): str(con.body)})
+
+# <codecell>
+
+minels
+
+# <codecell>
+
+noizjson = json.dumps(dausr)
+
+# <codecell>
+
+newposts = open('userurl.json', 'a')
+newposts.write(newzjson)
+print ('file userurl.json updated')
+newcomments = open('idcomt.json', 'a')
+newcomments.write(noizjson)
+print ('user comments updated')
+newposts.close()
+newcomments.close()
+
+# <codecell>
+
+rdusr = str(con.author)
+
+# <codecell>
+
+minelsz = []
+
+# <codecell>
+
+ls
+
+# <codecell>
+
+#for mina in minels:
+    #print mina.body
+   # minelsz.append(mina.body)
+
+# <codecell>
+
+#minelsz
+
+# <codecell>
+
+#mina.body
+
+# <codecell>
+
+cat idcomt.json
+
+# <codecell>
+
+
